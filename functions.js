1373d046b43a5a2255de23b99e73d299b45d2d99
SotiriaMpont <80161152+SotiriaMpont@users.noreply.github.com>
2022-01-04 20:10:17 +0200

    Add files via upload
    Θα πρέπει να υπάρχουν μέσα όλες οι συναρτήσεις που φτιάχνουμε !

---
+++ functions.js
@@ -0,0 +1,27 @@
+//ti tha ginei an pathsw to Submit 
+//function Submitclicked() {
+//pare to element apo to document me id= ExampleUsername  (username)
+//var usernameElement = document.getElementById('ExampleUsername');
+//pare twra thn timh tou 
+//var usernameValue = usernameElement.value;
+
+//console.log(usernameValue); //elegxos an douleuei
+
+//pare to element apo to document me id=ExamplePassword (password)
+// var passwordElement = document.getElementById('ExamplePassword');
+//var passwordValue = passwordElement.value;
+
+//stelnw server ixix
+//send(server, usernameValue, passwordValue);
+
+//mou epistrefei server sth selida
+
+// var DataLogin = {
+//  username : usernameValue ,
+// password : passwordValue
+//  }
+
+//  console.log(Datalogin)
+// axios.post('./login', DataLogin);
+
+//}
