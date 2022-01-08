92a77cf22a32bda96744a49de4863f13c4d85ea7
SotiriaMpont <swtiriamp9@gmail.com>
2022-01-08 17:02:18 +0200

    commit

--- functions.js
+++ functions.js
@@ -2,11 +2,11 @@ const { send } = require("express/lib/response");
 
 function SubmitClicked() { // Otan pataw to submit sto login 
 
-    //pare apo to to documment thn timi apo to elemenent me id=ExampleUsername
+    // pare apo to to documment thn timi apo to elemenent me id=ExampleUsername
     let username = document.getElementById('ExampleUsername').value;
     let password = document.getElementById('ExamplePassword').value;
 
-    send(server,username,password)
+    send(server, username, password)
 
 
 
