eb6359008d0cad2195db4b3362b838a0d62f5b41
SotiriaMpont <80161152+SotiriaMpont@users.noreply.github.com>
2022-01-23 19:22:54 +0200

    Update PoiService.js

--- Service/PoiService.js
+++ Service/PoiService.js
@@ -5,6 +5,15 @@ class PoiService {
 
     async findbyid(id) {
         return await this.#repo.findbyid(id);
+        
+        
+         #RepoFood = new PoiRepository();
+
+    async findbyType(types) {
+
+        const types = await this.#RepoFood.FindbyUserName(types);
+        return await this.#RepoFood.findbyType(type);
+    }
     }
 }
 module.exports = PoiService;
